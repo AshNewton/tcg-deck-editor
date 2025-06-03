5873f70ac58f616b94b0e879876cb79292a689ce
Ashley Newton <ash-newton@outlook.com>
2025-06-03 11:09:43 -0400

    replace mtg api with scryfall

--- src/decklist/api/magicthegathering.ts
+++ src/decklist/api/magicthegathering.ts
@@ -2,11 +2,11 @@ import { mtgCard } from "../../types";
 
 // https://docs.magicthegathering.io/#advancedcards_get_by_name
 
-const searchCardsUrl = "https://api.magicthegathering.io/v1/cards?";
+const searchCardsUrl = "https://api.scryfall.com/cards/search?";
 
 export const searchCard = async (name: String): Promise<Array<mtgCard>> => {
   try {
-    const response = await fetch(searchCardsUrl + "name=" + name);
+    const response = await fetch(searchCardsUrl + "q=" + name);
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
@@ -14,20 +14,10 @@ export const searchCard = async (name: String): Promise<Array<mtgCard>> => {
     const raw = await response.json();
 
     // format the result from searchCard into how we format the decks
-
-    //remove duplicates because we dont care about sets right now
-    const seen = new Set<string>();
-    return raw.cards
-      .filter((card: mtgCard) => {
-        if (seen.has(card.name)) {
-          return false;
-        }
-        seen.add(card.name);
-        return true;
-      })
-      .map((card: mtgCard) => {
-        return { name: card.name, details: card, copies: 1 };
-      });
+    // https://scryfall.com/docs/api/cards/search    const seen = new Set<string>();
+    return raw.data.map((card: mtgCard) => {
+      return { name: card.name, details: card, copies: 1 };
+    });
   } catch (error: any) {
     throw new Error(`Error fetching data: ${error.message}`);
   }
