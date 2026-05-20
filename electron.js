df9ac03aa2832a5553b234b48e1e4f2d3b26937a
Ashley <ash-newton@outlook.com>
2026-05-19 21:15:55 -0400

    convert web app to desktop electron app

---
+++ electron.js
@@ -0,0 +1,48 @@
+console.log("Electron main process started");
+
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const isDev = process.defaultApp || !app.isPackaged;
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  const startUrl = isDev
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, '../build/index.html')}`;
+
+  mainWindow.loadURL(startUrl);
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
