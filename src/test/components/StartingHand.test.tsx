8304e502e3de3f5e921853e2434b69390161fb00
Ashley <ash-newton@outlook.com>
2026-06-27 16:50:51 -0400

    fix issues with jest

--- src/test/components/StartingHand.test.tsx
+++ src/test/components/StartingHand.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 
+import "@testing-library/jest-dom";
+
 import StartingHand from "../../decklist/components/StartingHand";
 
 import * as reduxHooks from "../../hooks";
